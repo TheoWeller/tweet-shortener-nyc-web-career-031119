160260a610c0a551904426b76fba37b6b7d0e86f
Theo Weller <weller.theodor@gmail.com>
2019-02-28 19:24:58 +0000

    Automatically backed up by Learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 #tweet = "Hey guys, can anyone teach me how to be cool? I really want to be the best at everything, you know what I mean? Tweeting is super fun you guys!!!!"
-
+myArray = ["hello", "to", "be", "for"]
 def dictionary
   {
     "hello" => "hi",
